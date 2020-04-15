7738106fcf45b7e6ffed45645ba01ef571160fb8
brownguo <yexuan.guo@codedance.cn>
2020-04-15 16:17:07 +0800

    delete files & mock json files

--- mock.php
+++ mock.php
@@ -1,4 +1,5 @@
 <?php
+    //mock 一些数据结构
     $ret = array(
         'code' => 200,
         'msg'  => 'success',
@@ -12,12 +13,23 @@
                 'uid'       => 9813761,
                 'nickname'  => 'BrownGuo'
             ),
-            'authList' => array(
-                'orders'=>array(
-                    'search' => true,
-                    'edit'   => true,
-                    'verify' => true,
-                ),
+            'authList' => array('orders_all_list','orders_detail_by_id'),
+
+            'orderList'=>array(
+                array(
+                    'id'                 =>'128173821831',
+                    'name'               => '杜甫',
+                    'mobile_no'          => '13111112222',
+                    'callNo'             => 2912,
+                    'status'             => 1,
+                    'create_at'          => date('Y-m-d H:i:s',time()),
+                    'order_verify_at'    => date('Y-m-d H:i:s',time()+30)
+                )
+            ),
+            'pagination' => array(
+                'total'     =>  1,
+                'pageSize'  =>  10,
+                'current'   =>  1
             )
         )
     );
